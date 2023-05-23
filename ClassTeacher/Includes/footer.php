f1e24bd3723b812d7e34fa517ee8eb568ffa5ed9
Tushar Thite <111340880+tusharthite15@users.noreply.github.com>
2023-05-23 08:03:44 +0530

    Add files via upload

---
+++ ClassTeacher/Includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Developed for College Project
+               &copy; <script> document.write(new Date().getFullYear()); </script> 
+            </span>
+          </div>
+        </div>
+      </footer>
